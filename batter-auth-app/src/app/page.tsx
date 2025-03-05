d52e2993de3fb9c55f7bdcb45a1c567926840f34
maruf <maruf84210cse@gmail.com>
2025-03-05 23:46:00 +0600

    feat : add session in home page

--- batter-auth-app/src/app/page.tsx
+++ batter-auth-app/src/app/page.tsx
@@ -1,10 +1,14 @@
 
 import LogOutUser from '@/components/serveClient/userLogout';
+import { useSession } from '@/lib/auth-client';
 
 export default function Home() {
+  const  data  = useSession;
+  console.log(data.name);
   return (
     <div className="flex flex-col justify-center items-center">
       <p className='py-4'>Home page</p>
+      {data ? <p>Logged in as {data.name}</p> : <p>Not logged in</p>}
       <LogOutUser />
     </div>
   );
